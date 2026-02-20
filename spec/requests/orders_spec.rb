8da0b16e992ba989102fee3e3087aaa44341cbaf
Felipe Pontes <fpontes321@gmail.com>
2026-02-20 19:21:16 -0300

    Add resources routes to orders and implement index endpoint with specs

---
+++ spec/requests/orders_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Orders", type: :request do
+  describe "GET /orders" do
+    it "returns a successful response" do
+      get orders_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
