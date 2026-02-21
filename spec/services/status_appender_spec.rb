2e6ca9eb199aeba776aed7de2dceb72d9823b5e5
Felipe Pontes <fpontes321@gmail.com>
2026-02-21 13:44:54 -0300

    Fix typo in the appender spec

--- spec/services/status_appender_spec.rb
+++ spec/services/status_appender_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe StatusAppender do
   describe '#append' do
-    let(:order) { create(:order, details: { 'statuses' => [], 'order_id' => order.id }) }
+    let(:order) { create(:order, details: { 'statuses' => [] }) }
     let(:appender) { StatusAppender.new }
 
     it 'appends a new status to the order' do
       result = appender.append(order.id, name: Order::STATUSES[:delivered], origin: 'STORE')
 
       expect(result.details['statuses'].length).to eq(2)
-      expect(result.details['statuses'].first['name']).to eq(Order::STATUSES[:delivered])
+      expect(result.details['statuses'].last['name']).to eq(Order::STATUSES[:delivered])
       expect(result.details['last_status_name']).to eq(Order::STATUSES[:delivered])
     end
   end
