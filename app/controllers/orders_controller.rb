ae9ff76cb199b102aaaf5ea1825219d145447816
Felipe Pontes <fpontes321@gmail.com>
2026-02-21 16:57:46 -0300

    Create `append_status` method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,12 @@ class OrdersController < ApplicationController
     @orders = Order.all
     render json: @orders
   end
+
+  def append_status
+    order = Order.find(params[:id])
+    state_machine = StateMachine.new
+    state_machine.transition_order(order.details['last_status_name'], order.id)
+    order.reload
+    render json: order
+  end
 end
