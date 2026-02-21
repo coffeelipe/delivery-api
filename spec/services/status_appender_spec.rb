9f9df14785ef007b623b14f03ae335bf93fc23fa
Felipe Pontes <fpontes321@gmail.com>
2026-02-21 17:25:33 -0300

    Implement spec to assert appending follows expected flow of states

--- spec/services/status_appender_spec.rb
+++ spec/services/status_appender_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StatusAppender do
@@ -11,6 +13,28 @@ RSpec.describe StatusAppender do
       expect(result.details['statuses'].length).to eq(2)
       expect(result.details['statuses'].last['name']).to eq(Order::STATUSES[:delivered])
       expect(result.details['last_status_name']).to eq(Order::STATUSES[:delivered])
+    end
+
+    it 'appends and updates in the correct flow until terminal state' do
+      # Order starts with RECEIVED (from after_create callback)
+      expected_flow = [Order::STATUSES[:confirmed], Order::STATUSES[:dispatched], Order::STATUSES[:delivered]]
+
+      expected_flow.each do |status|
+        result = appender.append(order.id, name: status, origin: 'STORE')
+        order.reload # Reload to see updated data
+        puts order.details['statuses'].map { |s| s['name'] }.join(' -> ')
+        expect(result.details['last_status_name']).to eq(status)
+        expect(order.details['statuses'].last['name']).to eq(status)
+      end
+
+      # Verify complete flow:
+      expect(order.details['statuses'].length).to eq(4)
+      expect(order.details['statuses'].map { |s| s['name'] }).to eq([
+                                                                      Order::STATUSES[:received],
+                                                                      Order::STATUSES[:confirmed],
+                                                                      Order::STATUSES[:dispatched],
+                                                                      Order::STATUSES[:delivered]
+                                                                    ])
     end
   end
 end
