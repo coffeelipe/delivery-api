6f21e57871580fe6466e86fcdd5de3b6b43f9c64
Felipe Pontes <fpontes321@gmail.com>
2026-02-21 17:33:45 -0300

    add `show` and `orders/:id/status` routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-  resources :orders, only: [:index]
+  resources :orders, only: %i[index show] do
+    member do
+      patch 'status', to: 'orders#append_status'
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
